318e32a948f5918944082bf3ce1a846e07634ca0
shreyashrestha <sshr0033@student.monash.edu>
2025-08-31 04:15:59 +1000

    Updated the assignment only for the login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const LoginPage = () => import('../views/LoginPage.vue')
-const SignupPage = () => import('../views/SignupPage.vue')
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', name: 'Login', component: LoginPage },
-    { path: '/signup', name: 'SignUp', component: SignupPage },
     { path: '/:pathMatch(.*)*', redirect: '/login' },
   ],
 })
